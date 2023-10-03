d2c782d36485f14017e55c8038f8586d671c1b60
Xi Yu <30382875+Cecilia831@users.noreply.github.com>
2023-10-02 17:31:04 -0700

    Update server.c

--- server.c
+++ server.c
@@ -2,16 +2,7 @@
 
 AUTHOR: Abhijeet Rastogi (http://www.google.com/profiles/abhijeet.1989)
 
-This is a very simple HTTP server. Default port is 10000 and ROOT for the server is your current working directory..
-
-You can provide command line arguments like:- $./a.out -p [port] -r [path]
-
-for ex. 
-$./a.out -p 50000 -r /home/
-to start a server at port 50000 with root directory as "/home"
-
-$./a.out -r /home/shadyabhi
-starts the server at port 10000 with ROOT as /home/shadyabhi
+Editored by Xi Yu
 
 */
 #include<stdio.h>
